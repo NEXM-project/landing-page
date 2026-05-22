086263b0fb1cd7a2e16283e44e9919b8a4e9b883
Tanvir Hasan Serazy <amihasan420@gmail.com>
2026-05-22 23:47:44 +0600

    feat(leadership): enhance LeadershipIntro with animations and SplitText component; update transition duration

--- src/components/home/LeadershipIntro.tsx
+++ src/components/home/LeadershipIntro.tsx
@@ -4,6 +4,10 @@ import { ImageWithFallback } from "../common/ImageWIthFallback";
 import Link from "next/link";
 import { useEffect, useRef } from "react";
 import { CountUp } from "countup.js";
+import { motion } from "motion/react";
+import { fadeInUp } from "@/utils/transition";
+import SplitText from "../SplitText";
+import { ArrowUpRight } from "lucide-react";
 
 const statsData = [
   { end: 40, suffix: "+", description: "Startups Supported" },
@@ -45,8 +49,16 @@ export const LeadershipIntro = () => {
   }, []);
 
   return (
-    <div className="bg-[#f8f9f8] py-10 lg:py-20" ref={containerRef}>
-      <div className="max-w-7xl w-full mx-auto px-4 flex flex-col xl:flex-row gap-4 xl:gap-10">
+    <div
+      className="min-h-screen bg-[#f8f9f8] flex flex-col justify-center items-center py-12"
+      ref={containerRef}
+    >
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        variants={fadeInUp}
+        className="max-w-7xl w-full mx-auto px-4 flex flex-col xl:flex-row gap-4 xl:gap-10"
+      >
         <div className="xl:max-h-140 w-full xl:w-1/2 ">
           <ImageWithFallback
             src="/images/leader-intro.jpg"
@@ -54,30 +66,64 @@ export const LeadershipIntro = () => {
             width={600}
             height={400}
             fallbackSrc={fallbackImage}
+            loading="eager"
             className="w-full h-1/2 xl:h-full object-cover rounded-md"
           />
         </div>
         <div className="w-full xl:w-1/2">
-          <h3 className="text-center text-2xl lg:text-5xl font-semibold ">
-            Meet Our CTO
-          </h3>
-          <p className="mt-4 lg:mt-10 text-justify text-gray-700 lg:text-xl">
+          <SplitText
+            text="Meet Our CTO"
+            className="text-center text-2xl lg:text-5xl font-semibold"
+            delay={50}
+            duration={1.25}
+            ease="power3.out"
+            splitType="chars"
+            from={{ opacity: 0, y: 40 }}
+            to={{ opacity: 1, y: 0 }}
+            threshold={0.1}
+            rootMargin="-100px"
+            textAlign="center"
+          />
+
+          <p className="mt-4 lg:mt-10 text-gray-700 lg:text-xl">
             Our CTO, John Doe, is a visionary leader with over 20 years of
-            experience in the tech industry. He has a proven track record of
+            experience in the tech industry. He has a prove track record of
             driving innovation and leading successful teams to deliver
             cutting-edge solutions. Under his leadership, our company has
             achieved significant milestones and continues to push the boundaries
             of technology. John's passion for technology and commitment to
-            excellence inspire our entire team to strive for greatness every
-            day.
+            excellence inspire our entire team to strive for greatness everyday.
           </p>
 
-          <Link
-            href="#"
-            className="mt-6 lg:mt-20 inline-block bg-primary text-center hover:bg-violet-900 text-white font-medium py-2 px-4 rounded-full transition-colors duration-300"
-          >
-            Learn More About Our Leadership
-          </Link>
+          {/* <SplitText
+            text="Our CTO, John Doe, is a visionary leader with over 20 years of experience in the tech industry. He has a prove track record of driving innovation and leading successful teams to deliver cutting-edge solutions. Under his leadership, our company has achieved significant milestones and continues to push the boundaries of technology. John's passion for technology and commitment to excellence inspire our entire team to strive for greatness everyday."
+            className="mt-4 lg:mt-10 text-gray-700 lg:text-xl"
+            delay={50}
+            duration={1.25}
+            ease="power3.out"
+            splitType="lines"
+            from={{ opacity: 0, y: 40 }}
+            to={{ opacity: 1, y: 0 }}
+            threshold={0.1}
+            rootMargin="-100px"
+            textAlign="left"
+          /> */}
+
+          <div className="mt-10 lg:mt-12">
+            <Link
+              href="#"
+              className="group relative inline-flex items-center gap-2 px-3 md:px-6 py-2 md:py-3 overflow-hidden bg-linear-to-b from-primary to-violet-900 rounded-full border border-primary text-white font-medium cursor-pointer"
+            >
+              <span className="relative z-10 text-center md:text-start transition-colors duration-600 group-hover:text-primary">
+                Learn More About Our Leaders
+              </span>
+              <ArrowUpRight className="group-hover:text-primary duration-600 transition-colors z-10" />
+              <span
+                className="absolute bottom-0 left-0 right-0 h-0 bg-white z-0 transition-all duration-500 group-hover:h-full"
+                style={{ transformOrigin: "50% 100% 0px" }}
+              ></span>
+            </Link>
+          </div>
 
           <div className="flex flex-col xl:flex-row gap-4 mt-6 lg:mt-12">
             <div className="lg:border-r lg:border-gray-300  px-2 flex gap-2 justify-center">
@@ -121,7 +167,7 @@ export const LeadershipIntro = () => {
             </div>
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
--- src/utils/transition.ts
+++ src/utils/transition.ts
@@ -12,6 +12,6 @@ export const fadeInUp = {
   visible: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.6, ease: cubicBezier(0.4, 0, 0.2, 1) },
+    transition: { duration: 0.9, ease: cubicBezier(0.4, 0, 0.2, 1) },
   },
 };
