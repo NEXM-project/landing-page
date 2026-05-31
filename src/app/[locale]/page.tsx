704faf303afbd71b3906c732d7122fba715b8cc4
Raf-PC <110781737+mhrafin@users.noreply.github.com>
2026-05-31 13:26:39 +0600

    chore: format workspace and render HomeReview component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -7,6 +7,7 @@ import { HomeFAQ } from "@/components/home/HomeFAQ";
 import { HomeNodiAI } from "@/components/home/HomeNodiAI";
 import { HomeOurTeam } from "@/components/home/HomeOurTeam";
 import { HomeProjectCycle } from "@/components/home/HomeProjectCycle";
+import { HomeReview } from "@/components/home/HomeReview";
 import { LeadershipIntro } from "@/components/home/LeadershipIntro";
 import { useTranslations } from "next-intl";
 
@@ -22,6 +23,7 @@ export default function Home() {
       <HomeEngineeringExcellence />
       <FeaturedProjects />
       <HomeProjectCycle />
+      <HomeReview />
       <HomeFAQ />
       <HomeContactUs />
     </>
