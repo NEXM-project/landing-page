9647569d30907ddf9189f36e74238944a7e4411b
Raf-PC <110781737+mhrafin@users.noreply.github.com>
2026-06-07 16:21:33 +0600

    feat(projects): add Arcon Interior project and update section copy

--- src/components/home/HomeProjects.tsx
+++ src/components/home/HomeProjects.tsx
@@ -58,7 +58,7 @@ export const HomeProjects = () => {
     <section className="max-w-7xl mx-auto px-4 py-20">
       <div>
         <h2 className="text-5xl font-semibold text-center mb-8">
-          Our Projects
+          Affiliated Projects
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ">
           {projectData.map((project: Project, index: number) => (
